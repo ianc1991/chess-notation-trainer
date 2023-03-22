817255d46c7917a514e5efffa6f308b61251f61f
Ian Christian <ianmchristian@gmail.com>
2023-03-22 09:13:05 -0400

    added board

---
+++ chess-notation-trainer/src/components/board/Board.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { Chessground } from 'chessground'
+import 'chessground/assets/chessground.base.css'
+import 'chessground/assets/chessground.brown.css'
+import 'chessground/assets/chessground.cburnett.css'
+import { onMounted, ref } from 'vue'
+
+const boardContainer = ref()
+
+onMounted(() => {
+  const chessground = Chessground(boardContainer.value, {
+    // Options for Chessground go here
+    // The initial position of the pieces
+    fen: 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR',
+  });
+
+})
+</script>
+
+<!-- T E M P L A T E -->
+<template>
+<div ref="boardContainer"></div>
+</template>
+
+<!-- S T Y L E S -->
+<style scoped>
+</style>
