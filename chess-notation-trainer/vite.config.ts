4498817e0861d04713411f4f57b97b9a940dfa1c
Ian Christian <ianmchristian@gmail.com>
2023-04-04 16:16:23 -0400

    uninstalled some things

--- chess-notation-trainer/vite.config.ts
+++ chess-notation-trainer/vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    watch: {
-      ignored: ['!**/node_modules/your-package-name/**'],
-    },
-  }
+	plugins: [vue()],
+
+	server: {
+		watch: {
+			ignored: ['!**/node_modules/your-package-name/**'],
+		},
+	},
 })
