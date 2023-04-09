bb4cc2f6bfa3752d268a97cceaf838be49120329
Ian Christian <ianmchristian@gmail.com>
2023-04-09 11:35:59 -0400

    removed dupe folder

--- chess-notation-trainer/src/components/Reusables/Button/Primary/PrimaryButton.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-	const props = defineProps({
-		text: String,
-	})
-</script>
-
-<template>
-	<button class="px-4 py-2 text-white rounded-full bg-sky-500">{{ text }}</button>
-</template>
